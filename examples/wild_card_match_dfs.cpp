14cf04c50e82a87e1fe46b3056bf15f74dd7df32
Johannes Bader <jogus@live.de>
2015-06-10 15:40:07 +0200

    Restructured test program

--- examples/wild_card_match_dfs.cpp
+++ examples/wild_card_match_dfs.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 using namespace std::chrono;
 using timer = std::chrono::high_resolution_clock;
-
+ 
 
 template <typename type_csa, typename type_wt>
 size_t match_ref(type_csa csa, type_wt wts, string s1, string s2, size_t min_gap, size_t max_gap, std::function<void(typename type_csa::size_type a, typename type_csa::size_type b)> callback)
@@ -26,8 +26,6 @@ size_t match_ref(type_csa csa, type_wt wts, string s1, string s2, size_t min_gap
     if (cnt1 == 0 || cnt2 == 0)
         return 0;
 
-    size_type pat_len = s1.size();
-
     for (auto i2 = sp2; i2 <= ep2; i2++)
         p2.push_back(wts[i2]);
 
@@ -37,23 +35,23 @@ size_t match_ref(type_csa csa, type_wt wts, string s1, string s2, size_t min_gap
     for (auto i1 = sp1; i1 <= ep1; i1++)
     {
         size_type l1 = wts[i1];
-        auto i2a = lower_bound(p2.begin(), p2.end(), l1 + min_gap + pat_len);
-        auto i2b = upper_bound(p2.begin(), p2.end(), l1 + max_gap + pat_len);
+        auto i2a = lower_bound(p2.begin(), p2.end(), l1 + min_gap);
+        auto i2b = upper_bound(p2.begin(), p2.end(), l1 + max_gap);
 
         for (auto i2 = i2a; i2 < i2b; i2++)
         {
-            callback(l1, *i2 + s2.size() - 1);
+            callback(l1, *i2);
             result++;
-        }
+        } 
     }
     return result;
 }
-
+ 
 template <typename type_matching_index>
 size_t match_dfs(type_matching_index& index, string s1, string s2, size_t min_gap, size_t max_gap, std::function<void(typename type_matching_index::size_type a, typename type_matching_index::size_type b)> callback)
 {
     size_t cnt = 0;
-    for (auto res : index.match2(incremental_wildcard_pattern(s1, min_gap, max_gap), s2))
+    for (auto res : index.match2(s1, incremental_wildcard_pattern(s2, min_gap, max_gap)))
     {
         callback(res.first, res.second);
         ++cnt;
@@ -63,28 +61,13 @@ size_t match_dfs(type_matching_index& index, string s1, string s2, size_t min_ga
 
 template <typename type_matching_index>
 size_t match_dfs_ex(type_matching_index& index, 
-    incremental_wildcard_pattern p1, 
-    string s)
+    string s, 
+    incremental_wildcard_pattern p1)
 {
     size_t cnt = 0;
-    for (auto res : index.match2(p1, s))
+    for (auto res : index.match2(s, p1))
     {
         cout << res.first << " " << res.second << endl;
-        ++cnt;
-    }
-    return cnt;
-}
-
-template <typename type_matching_index>
-size_t match3_dfs(type_matching_index& index, 
-    incremental_wildcard_pattern p1, 
-    incremental_wildcard_pattern p2, 
-    string s)
-{
-    size_t cnt = 0;
-    for (vector<size_t> res : index.match3(p1, p2, s))
-    {
-        cout << res[0] << " " << res[1] << " " << res[2] << endl;
         ++cnt;
     }
     return cnt;
@@ -125,30 +108,35 @@ int main(int argc, char* argv[])
             cout << "\t" << extract(csa, a, b) << endl; 
     };
     std::function<void(size_type a, size_type b)> callback_dbg = [&](size_type a, size_type b) 
-    { 
+    {
         cout << "\t" << a << " " << b << endl; 
     };
     std::function<void(size_type a, size_type b)> callback_nop = [](size_type a, size_type b) { (void)a; (void)b; };
 
-    bool test_and_bench = true;
-    if (test_and_bench) 
+    auto compare = [&](string s1, string s2, size_t min_gap, size_t max_gap) 
+    { 
+        cout << "Comparing with: s1=" << s1 << ", s2=" << s2 << ", min_gap=" << min_gap << ", max_gap=" << max_gap << endl;
+        
+        size_t matches = match_dfs(index, s1, s2, min_gap, max_gap, callback_cout);
+        size_t matches_ref = match_ref(csa, wts, s1, s2, min_gap, max_gap, callback_nop);
+            
+        bool success = matches == matches_ref;
+        if (!success)
+        {
+            cout << "Error" << endl;
+            cout << "MATCHES:     " << matches << endl;
+            cout << "MATCHES_ref: " << matches_ref << endl;
+        }
+        else
+            cout << matches << " found" << endl;
+        
+        return success;
+    };
+    
+    bool test_and_bench = false;
+    if (test_and_bench)
     {
         // TESTS
-        auto test = [&](string s1, string s2, size_t min_gap, size_t max_gap) 
-        { 
-            size_t matches = match_dfs(index, s1, s2, min_gap, max_gap, callback_nop);
-            size_t matches_ref = match_ref(csa, wts, s1, s2, min_gap, max_gap, callback_nop);
-            
-            bool success = matches == matches_ref;
-            if (!success)
-            {
-                cout << "Error on: s1=" << s1 << ", s2=" << s2 << ", min_gap=" << min_gap << ", max_gap=" << max_gap << endl;
-                cout << "MATCHES:     " << matches << endl;
-                cout << "MATCHES_ref: " << matches_ref << endl;
-            }
-            
-            return success;
-        };
         vector<pair<string, string>> tcs;
         tcs.emplace_back("8", "8");
         tcs.emplace_back("8", "0");
@@ -160,17 +148,17 @@ int main(int argc, char* argv[])
             auto s1 = tcs[tci].first;
             auto s2 = tcs[tci].second;
             cout << "TEST CASE: " << s1 << " " << s2 << endl;
-            for (int i = 0; i <= max; i += (rand() % 2) + 1)
+            for (int i = 0; i <= max; i += 3)
             { 
-                if (!test(s1, s2, i, max)) goto err;
-                if (!test(s1, s2, 0, i)) goto err;
-                if (!test(s1, s2, i, i)) goto err;
-                if (!test(s1, s2, i, 2*i)) goto err;
-                if (!test(s1, s2, 10 * i, 10 * i + 3)) goto err;
-                if (!test(s1, s2, 10 * i, 10 * i + 10)) goto err;
+                if (!compare(s1, s2, i, max)) goto err;
+                if (!compare(s1, s2, 0, i)) goto err;
+                if (!compare(s1, s2, i, i)) goto err;
+                if (!compare(s1, s2, i, 2*i)) goto err;
+                if (!compare(s1, s2, 10 * i, 10 * i + 3)) goto err;
+                if (!compare(s1, s2, 10 * i, 10 * i + 10)) goto err;
             }
         }
-        
+         
         // BENCH
         {
             size_t found = 0;
@@ -193,7 +181,7 @@ int main(int argc, char* argv[])
             cout << "BENCH_DFS: " << duration_cast<milliseconds>(stop-start).count() << "ms for " << found << " occurrences" << endl;
         
             found = 0;
-            start = timer::now(); 
+            start = timer::now();
             for (unsigned int tci = 0; tci < tcs.size(); tci++)
             {
                 auto s1 = tcs[tci].first;
@@ -214,21 +202,27 @@ int main(int argc, char* argv[])
     }
 
     // PROMPT
-    {
-        size_t min1_gap = 0;
-        size_t max1_gap = 0;
-        size_t min2_gap = 0;
-        size_t max2_gap = 0;
+    { 
+        size_t min1_gap;
+        size_t max1_gap;
+        //size_t min2_gap;
+        //size_t max2_gap;
         string s1, s2, s3;
         cout << "PROMPT: " << endl;
-        //while (cin >> s1 >> s2 >> min_gap >> max_gap)
-        //    cout << match_dfs(index, s1, s2, min_gap, max_gap, callback_cout) << " matches found" << endl;
-        while (cin >> s1 >> min1_gap >> max1_gap >> s2 >> min2_gap >> max2_gap >> s3)
-            cout << match3_dfs(index, 
-                incremental_wildcard_pattern(s1, min1_gap, max1_gap),
-                incremental_wildcard_pattern(s2, min2_gap, max2_gap),
-                s3
-                ) << " matches found" << endl;
+        while (cin >> s1 >> s2 >> min1_gap >> max1_gap)
+            compare(s1, s2, min1_gap, max1_gap);
+            //cout << match_dfs(index, s1, s2, min1_gap, max1_gap, callback_cout) << " matches found" << endl;
+        //while (cin >> s1 >> min1_gap >> max1_gap >> s2 >> min2_gap >> max2_gap >> s3)
+        //    cout << match3_dfs(index, 
+        //        s1,
+        //        incremental_wildcard_pattern(s2, min1_gap + s1.size(), max1_gap + s1.size()),
+        //        incremental_wildcard_pattern(s3, min2_gap + s2.size(), max2_gap + s2.size())
+        //        ) << " matches found" << endl;
+        //while (cin >> s1 >> s2 >> min1_gap >> max1_gap >> s3)
+        //    cout << match_dfs_ex(index, 
+        //        s1,
+        //        incremental_wildcard_pattern(s2, min1_gap + s1.size(), max1_gap + s1.size(), s3)
+        //        ) << " matches found" << endl;
     }
     err:;
 }
--- include/sdsl/matching.hpp
+++ include/sdsl/matching.hpp
@@ -45,16 +45,11 @@ struct incremental_wildcard_pattern
     const size_t max_gap;
     const string except;
 
-    const size_t total_min_size;
-    const size_t total_max_size;
-
     incremental_wildcard_pattern() : 
             pattern(""), 
             min_gap(0),
             max_gap(0),
-            except(""),
-            total_min_size(0),
-            total_max_size(0)
+            except("")
     { }
 
     incremental_wildcard_pattern(
@@ -65,9 +60,7 @@ struct incremental_wildcard_pattern
             pattern(pattern),
             min_gap(min_gap),
             max_gap(max_gap),
-            except(except),
-            total_min_size(pattern.size() + min_gap),
-            total_max_size(pattern.size() + max_gap)
+            except(except)
     {
     }
 };
@@ -151,7 +144,6 @@ private:
     deque<size_t> b_values;
 
     incremental_wildcard_pattern p1;
-    size_t s_size;
 
     result_type current;
 
@@ -170,10 +162,10 @@ private:
     void adjust_b_range()
     {
         // shrink
-        while (!b_values.empty() && a + p1.total_min_size > b_values.front())
+        while (!b_values.empty() && a + p1.min_gap > b_values.front())
             b_values.pop_front();
         // expand
-        while (!lex_ranges[1].empty() && a + p1.total_max_size >= lex_ranges[1].top().second->range_begin)
+        while (!lex_ranges[1].empty() && a + p1.max_gap >= lex_ranges[1].top().second->range_begin)
         {
             if (lex_ranges[1].top().second->is_leaf)
             {                    
@@ -192,7 +184,7 @@ private:
         // find next connected match
         while (!lex_ranges[0].empty() 
             && !b_values.empty()
-            && lex_ranges[0].top().second->range_begin + p1.total_min_size <= b_values.back())
+            && lex_ranges[0].top().second->range_begin + p1.min_gap <= b_values.back())
         {
             if (lex_ranges[0].top().second->is_leaf)
             {
@@ -211,9 +203,9 @@ private:
         {
             const auto& top0 = lex_ranges[0].top().second;
             const auto& top1 = lex_ranges[1].top().second;
-            if (top0->range_end + p1.total_max_size < top1->range_begin)
+            if (top0->range_end + p1.max_gap < top1->range_begin)
                 lex_ranges[0].pop();
-            else if (top0->range_begin + p1.total_min_size > top1->range_end)
+            else if (top0->range_begin + p1.min_gap > top1->range_end)
                 lex_ranges[1].pop();
             else if (top0->is_leaf && top1->is_leaf)
             {
@@ -238,7 +230,7 @@ private:
         if (!valid() && !next_batch())
             return; // end
 
-        current = make_pair(a, b_values[b_idx]+s_size-1);
+        current = make_pair(a, b_values[b_idx]);
     }
 
 public:
@@ -246,17 +238,15 @@ public:
     {
     }
     wild_card_match_iterator(const type_csa& csa, const type_wt& wts, 
-        incremental_wildcard_pattern p1, 
-        string s) 
+        string s, 
+        incremental_wildcard_pattern p1) 
         : wts(wts), p1(p1)
     {
-        s_size = s.size();
-        
         auto root_node = make_shared<node_cache<type_csa, type_wt>>(this->wts.root(), &this->wts);
         size_type sp = 1, ep = 0;
-        if (0 != backward_search(csa, 0, csa.size()-1, p1.pattern.begin(), p1.pattern.end(), sp, ep))
-            lex_ranges[0].emplace(range_type(sp, ep),root_node);
         if (0 != backward_search(csa, 0, csa.size()-1, s.begin(), s.end(), sp, ep))
+            lex_ranges[0].emplace(range_type(sp, ep),root_node);
+        if (0 != backward_search(csa, 0, csa.size()-1, p1.pattern.begin(), p1.pattern.end(), sp, ep))
             lex_ranges[1].emplace(range_type(sp, ep),root_node);
         if (p1.except != "")
             if (0 != backward_search(csa, 0, csa.size()-1, p1.except.begin(), p1.except.end(), sp, ep))
@@ -308,201 +298,6 @@ public:
     }
 };
 
-template<class type_csa, class type_wt>
-class wild_card_match_iterator3 : public std::iterator<std::forward_iterator_tag, vector<typename type_csa::size_type>>
-{
-private:
-    typedef typename type_wt::node_type  node_type;
-    typedef typename type_csa::size_type size_type;
-    typedef vector<size_type>            result_type;
-
-    // (lex_range, node)
-    array<stack<pair<range_type,shared_ptr<node_cache<type_csa, type_wt>>> >, 3> lex_ranges;
-
-    type_wt wts;
-    size_t a;
-    size_t b_idx = 0;
-    deque<size_t> b_values;
-    size_t c_idx = -1;
-    deque<size_t> c_values;
-
-    incremental_wildcard_pattern p1;
-    incremental_wildcard_pattern p2;
-    size_t s_size;
-
-    result_type current;
-
-    void split_node(int t)
-    {
-        auto top = lex_ranges[t].top(); lex_ranges[t].pop();
-        auto& node = top.second;
-        node->ensure_children();
-        auto exp_range = wts.expand(node->node, top.first);
-        if (!empty(exp_range[1]))
-            lex_ranges[t].emplace(exp_range[1], node->children.second);
-        if (!empty(exp_range[0]))
-            lex_ranges[t].emplace(exp_range[0], node->children.first);
-    };
-    
-    void adjust_ranges()
-    {
-        // shrink
-        while (!b_values.empty() && a + p1.total_min_size > b_values.front())
-            b_values.pop_front();
-        // expand
-        while (!lex_ranges[1].empty() && a + p1.total_max_size >= lex_ranges[1].top().second->range_begin)
-        {
-            if (lex_ranges[1].top().second->is_leaf)
-            {                    
-                b_values.push_back(lex_ranges[1].top().second->range_begin);
-                lex_ranges[1].pop();
-            }
-            else
-                split_node(1);
-        }
-        
-        // shrink
-        while (!c_values.empty() && a + p1.total_min_size + p2.total_min_size > c_values.front())
-            c_values.pop_front();
-        // expand
-        while (!lex_ranges[2].empty() && a + p1.total_max_size + p2.total_max_size >= lex_ranges[2].top().second->range_begin)
-        {
-            if (lex_ranges[2].top().second->is_leaf)
-            {                    
-                c_values.push_back(lex_ranges[2].top().second->range_begin);
-                lex_ranges[2].pop();
-            }
-            else
-                split_node(2);
-        }
-    }
-
-    bool next_batch()
-    {
-        b_idx = 0;
-        
-        // find next connected match
-        while (!lex_ranges[0].empty() 
-            && !b_values.empty()
-            && lex_ranges[0].top().second->range_begin + p1.total_min_size <= b_values.back())
-        {
-            if (lex_ranges[0].top().second->is_leaf)
-            {
-                a = lex_ranges[0].top().second->range_begin;
-                lex_ranges[0].pop();
-                adjust_ranges();
-                if (!b_values.empty())
-                    return true;
-            }
-            else
-                split_node(0);
-        }
-            
-        // find next independent match
-        while (!lex_ranges[0].empty() && !lex_ranges[1].empty())
-        {
-            const auto& top0 = lex_ranges[0].top().second;
-            const auto& top1 = lex_ranges[1].top().second;
-            if (top0->range_end + p1.total_max_size < top1->range_begin)
-                lex_ranges[0].pop();
-            else if (top0->range_begin + p1.total_min_size > top1->range_end)
-                lex_ranges[1].pop();
-            else if (top0->is_leaf && top1->is_leaf)
-            {
-                a = top0->range_begin;
-                b_values.push_back(top1->range_begin);
-                lex_ranges[0].pop();
-                lex_ranges[1].pop();
-                adjust_ranges();
-                return true;
-            }
-            else
-                split_node(top1->range_size() > top0->range_size() ? 1 : 0);
-        }
-
-        b_values.clear();
-        return false;
-    }
-
-    void next()
-    {
-        ++c_idx;
-        if (c_idx < c_values.size())
-        {
-            c_idx = 0;
-        }
-        if (!valid() && !next_batch())
-            return; // end
-
-        current = { a, b_values[b_idx], c_values[c_idx] };
-    }
-
-public:
-    wild_card_match_iterator3()
-    {
-    }
-    wild_card_match_iterator3(const type_csa& csa, const type_wt& wts,
-        incremental_wildcard_pattern p1, 
-        incremental_wildcard_pattern p2,
-        string s) : wts(wts), p1(p1), p2(p2)
-    {
-        s_size = s.size();
-        
-        auto root_node = make_shared<node_cache<type_csa, type_wt>>(this->wts.root(), &this->wts);
-        size_type sp = 1, ep = 0;
-        if (0 != backward_search(csa, 0, csa.size()-1, p1.pattern.begin(), p1.pattern.end(), sp, ep))
-            lex_ranges[0].emplace(range_type(sp, ep),root_node);
-        if (0 != backward_search(csa, 0, csa.size()-1, p2.pattern.begin(), p2.pattern.end(), sp, ep))
-            lex_ranges[1].emplace(range_type(sp, ep),root_node);
-        if (0 != backward_search(csa, 0, csa.size()-1, s.begin(), s.end(), sp, ep))
-            lex_ranges[2].emplace(range_type(sp, ep),root_node);
-
-        next();
-    }
-
-    bool valid() const
-    {
-        return c_idx < c_values.size();
-    }
-
-    result_type operator*() const
-    {
-        return current;
-    }
-    result_type* operator->()
-    {
-        return &current;
-    }
-
-    wild_card_match_iterator3& operator++()
-    {
-        next();
-        return *this;
-    }
-    
-    friend bool operator==(
-        const wild_card_match_iterator3& a,
-        const wild_card_match_iterator3& b)
-    {
-        if (!a.valid() && !b.valid())
-            return true;
-        if (!a.valid() || !b.valid())
-            return false;
-        if (a.a != b.a)
-            return false;
-        if (a.b_idx != b.b_idx)
-            return false;
-        return a.current == b.current;
-    }
-    
-    friend bool operator!=(
-        const wild_card_match_iterator3& a,
-        const wild_card_match_iterator3& b)
-    {
-        return !(a == b);
-    }
-};
-
 template<class t_csa=csa_wt<wt_huff<rrr_vector<63>>>, 
          class t_wt=wt_int<bit_vector, rank_support_v5<>, select_support_scan<1>, select_support_scan<0>>>
 class matching_index
@@ -536,22 +331,13 @@ public:
     }   
     
     matching_result<iterator> match2(
-        incremental_wildcard_pattern p1,
-        string s)
+        string s,
+        incremental_wildcard_pattern p1
+        )
     {
         return matching_result<iterator>(
-            wild_card_match_iterator<csa_type, wt_type>(csa, wt, p1, s),
+            wild_card_match_iterator<csa_type, wt_type>(csa, wt, s, p1),
             wild_card_match_iterator<csa_type, wt_type>());
-    }
-    
-    matching_result<wild_card_match_iterator3<csa_type, wt_type>> match3(
-        incremental_wildcard_pattern p1, 
-        incremental_wildcard_pattern p2,
-        string s)
-    {
-        return matching_result<wild_card_match_iterator3<csa_type, wt_type>>(
-            wild_card_match_iterator3<csa_type, wt_type>(csa, wt, p1, p2, s),
-            wild_card_match_iterator3<csa_type, wt_type>());
     }
 };
 
