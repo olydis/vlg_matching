fdf8351e5b7b4df88e08b2183db856ca30b1359c
Johannes Bader <jogus@live.de>
2015-08-18 13:33:25 +0200

    enabled word extractor to extract larger substrings efficiently

--- examples/generate-pattern.cpp
+++ examples/generate-pattern.cpp
@@ -8,19 +8,16 @@ int main(int argc, char** argv)
 {
     if (argc <= 3) 
     {
-        cout << "Usage: " << argv[0] << " file count length [mode [charset]]" << endl;
+        cout << "Usage: " << argv[0] << " file count length [charset]" << endl;
         cout << " - file:       The data to extract the patterns from" << endl;
         cout << " - count:      Number of patterns to extract" << endl;
         cout << " - length:     Minimum length of patterns to extract" << endl;
-        cout << " - mode:" << endl;
-        cout << "   - 0 = common (default)" << endl;
-        cout << "   - 1 = rare" << endl;
-        cout << "   - 2 = lexicographically" << endl;
         cout << " - charset:" << endl;
-        cout << "   - 0 = alpha-numerical only (default)" << endl;
+        cout << "   - 0 = alpha-numerical only" << endl;
         cout << "   - 1 = anything" << endl;
+        cout << "   - default = length >= 10" << endl;
         cout << endl;
-        cout << " Uses a CST to extract patterns/phrases matching provided criteria." << endl;
+        cout << " Uses a CST to extract most common patterns/phrases matching provided criteria." << endl;
         cout << endl;
         return 1;
     }
@@ -29,15 +26,14 @@ int main(int argc, char** argv)
     string file(argv[1]);
     size_t count = atoi(argv[2]);
     size_t length = atoi(argv[3]);
-    int mode = argc <= 4 ? 0 : atoi(argv[4]);
-    int charset = argc <= 5 ? 0 : atoi(argv[5]);
+    int charset = argc <= 4 ? (length >= 10 ? 1 : 0) : atoi(argv[4]);
     
     // CHARSET
     auto is_special_char = [](char c) { return isspace(c) || ispunct(c); };
 
     std::function<bool(char c)> charset_predicates[] = {
         [](char c) { return isalnum(c) || c == '_'; },
-        [](char c) { (void)c; return true; }
+        [](char c) { return c != '\n' && c != '\r' && c != '\t' && c != '\v'; }
     };
     auto charset_predicate = charset_predicates[charset];
     auto charset_string_predicate = [&](string s) { return all_of(s.begin(), s.end(), charset_predicate); };
@@ -50,15 +46,12 @@ int main(int argc, char** argv)
         construct(cst, file, 1);
         store_to_file(cst, cst_file);
     }
-    
+
     // traverse
     size_t size = cst.size();
-    size_t node_size_thresh = 1;
-    for (size_t i = 0; i < length; i++)
-        node_size_thresh *= 26;
-    
-    vector<pair<size_t, string>> found;
-    for (auto it=cst.begin(); it!=cst.end() && found.size() <= 1000*count; ++it) 
+    stack<pair<size_t, string>> found;
+    auto min_occ = [&]() { return found.empty() ? 0 : found.top().first; };
+    for (auto it=cst.begin(); it!=cst.end(); ++it) 
     {
         if (it.visit() == 1) 
         {
@@ -67,7 +60,7 @@ int main(int argc, char** argv)
             if (depth == 0)
                 continue;
             
-            if (cst.size(v) < size / node_size_thresh)
+            if (cst.size(v) <= min_occ())
             {
                 it.skip_subtree();
                 continue;
@@ -93,31 +86,32 @@ int main(int argc, char** argv)
                     continue;
                 }
 
-                size_t occ = 1 + cst.rb(v) - cst.lb(v);
-                found.emplace_back(occ, phrase);
+                size_t occ = cst.size(v);
+                stack<pair<size_t, string>> helper;
+                while (!found.empty() && found.top().first < occ)
+                {
+                    helper.push(found.top());
+                    found.pop();
+                }
+                found.emplace(occ, phrase);
+                while (found.size() < count && !helper.empty())
+                {
+                    found.push(helper.top());
+                    helper.pop();
+                }
                 it.skip_subtree();
             }
         }
     }
     
-    // sort
-    switch (mode)
-    {
-        case 0: // common
-            sort(found.rbegin(), found.rend());
-            break;
-        case 1: // rare
-            sort(found.begin(), found.end());
-            break;
-        case 2: // lexicographically
-            break;
-    }
-    
-    // head
-    if (found.size() > count)
-        found.resize(count);
-    
+    // fill
+    while (found.size() < count)
+        found.emplace(0, string(length, 'x'));    
+
     // output
-    for (auto result : found)
-        cout << result.second << endl;
+    while (!found.empty())
+    {
+        cout << found.top().second << endl;
+        found.pop();
+    }
 }
