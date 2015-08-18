5d167b25968c370e5cfcd085f7a4be98e10fb243
Johannes Bader <jogus@live.de>
2015-08-18 14:00:11 +0200

    made benachmark system more flexible

--- benchmark/gapped-matching/visualize/gm.R
+++ benchmark/gapped-matching/visualize/gm.R
@@ -4,7 +4,8 @@ source("../../basic_functions.R")
 tex_file = "gm.tex"
 
 # Load experiment information
-tc_config <- readConfig("../test_case.config",c("TC_ID","LATEX-NAME","PCH","LTY","COL"))
+algo_config <- readConfig("../algorithms.config",c("ALGO_ID","LATEX-NAME","PCH","LTY","COL"))
+tc_config <- readConfig("../test_case.config",c("TC_ID","SP-LEN","GAP"))
 
 open_tikz <- function( file_name  ){
     tikz(file_name, width = 5.5, height = 6, standAlone = F)
@@ -17,38 +18,35 @@ plot_gm_query_times <- function( data, title=""){
   #data[c('total_time_ms')] <- data[c('total_time_ms')]/1000.0
   #data <- data[order(data[['PATT_SAMPLE']]), ]
 
-  max_runtime <- max(1000 * data[['mean_time_ms']] / data[['num_results']])
-  max_sample <- max(data[['PATT_SAMPLE']])
+  runtime <- 1000 * data[['mean_time_ms']] / data[['num_results']]
+  max_runtime <- max(runtime[!is.infinite(runtime) & !is.nan(runtime)])
+  max_sample <- length(unique(data[['PATT_SAMPLE']]))
 
-    #plot(NA, NA, xlim=c(0,1),ylim=c(0,1),ylab="", xlab="", bty="n", type="n", yaxt="n", xaxt="n")
-    plot(NA, NA, type="l", xlim=c(0, max_sample ), ylim=c(0, max_runtime ),
-       xlab = "", ylab="", yaxt="n", xaxt="n"
-       )
+  plot(NA, NA, type="l", xlim=c(0, max_sample ), ylim=c(0, max_runtime ),
+       xlab = "", ylab="", yaxt="n", xaxt="n")
 
-  tc_ids <- unique(data$TC_ID)
-  for(tc in tc_ids){
-    d <- data[data$TC_ID==tc,]
+  ALGO_IDs <- unique(data$ALGO)
+  for(tc in ALGO_IDs){
+    d <- data[data$ALGO==tc,]
     lines(d[['PATT_SAMPLE']], 1000 * d[['mean_time_ms']] / d[['num_results']],
           lwd=1, type="b", 
-          pch=tc_config[tc,"PCH"], 
-          lty=tc_config[tc,"LTY"],
-          col=tc_config[tc,"COL"])
+          pch=algo_config[tc,"PCH"], 
+          lty=algo_config[tc,"LTY"],
+          col=algo_config[tc,"COL"])
     box("plot", col="grey")    
     axis( 1, at = axTicks(1), labels=T, mgp=c(2,0.5,0), tcl=-0.2, cex.axis=1, las=1 )
-
   }
 
-
-    mtext("Gap size", side=1, line=2, las=0)
-    axis( 2, at = axTicks(2),  mgp=c(1,0.3,0), tcl=-0.2, cex.axis=0.8 )
-    mtext("Average query time in ($\\mu s$)", side=2, line=2, las=0)
+  mtext("Test case", side=1, line=2, las=0)
+  axis( 2, at = axTicks(2),  mgp=c(1,0.3,0), tcl=-0.2, cex.axis=0.8 )
+  mtext("Average query time in ($\\mu s$)", side=2, line=2, las=0)
 
   grid(lty=1)  
   draw_figure_heading(sprintf("collection = %s",title))
 
   plot(NA, NA, xlim=c(0,1),ylim=c(0,1),ylab="", xlab="", bty="n", type="n", yaxt="n", xaxt="n")
-  legend( "top", legend=tc_config[tc_ids,"LATEX-NAME"], pch=tc_config[tc_ids,"PCH"], col=tc_config[tc_ids,"COL"],
-		    lty=tc_config[tc_ids,"LTY"], bty="n", y.intersp=1.5, ncol=2, title="Algorithm", cex=1.2)
+  legend( "top", legend=algo_config[ALGO_IDs,"LATEX-NAME"], pch=algo_config[ALGO_IDs,"PCH"], col=algo_config[ALGO_IDs,"COL"],
+		    lty=algo_config[ALGO_IDs,"LTY"], bty="n", y.intersp=1.5, ncol=2, title="Algorithm", cex=1.2)
 }
 
 data <- data_frame_from_key_value_pairs( "../results/all.txt" )
