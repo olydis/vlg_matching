a904b56bb93e3a4a3c842e7b19f319f2b80d865a
Johannes Bader <jogus@live.de>
2015-10-08 21:59:15 +0200

    let WCSEARCH_DFS switch to boost if SA-ranges pass threshold

--- benchmark/gapped-matching/include/index_wcsearch.hpp
+++ benchmark/gapped-matching/include/index_wcsearch.hpp
@@ -1,19 +1,20 @@
 #pragma once
 
-#include "utils.hpp"
 #include "collection.hpp"
-#include "sdsl/suffix_arrays.hpp"
+#include "utils.hpp"
 #include "sdsl/matching.hpp"
+#include "sdsl/suffix_arrays.hpp"
 
-//size_t sdsl::PERFCTR_NUM_PROCESSED_WT_NODES;
+#include <boost/regex.hpp>
 
-// THIS IS JUST A PROXY until a consistent API for indices is figured out
-// (or is the "collection"-approach final?)
+#define CONST_LINEAR_THRESH 35
+
 class index_wcsearch
 {
     private:
         typedef sdsl::matching_index<sdsl::wt_int<>, sdsl::rrr_vector<>> index_type;
         index_type index;
+        string text = "";
 
     public:
         typedef sdsl::int_vector<0>::size_type size_type;
@@ -53,15 +54,17 @@ class index_wcsearch
             // output SA-ranges (gives a good estimation about potential matches)
             index_type::size_type total_range = 0, sp = 0, ep = 0;
 
-            std::string s1;
-            std::string s2;
-
             for (size_t i = 0; i < pat.subpatterns.size(); ++i)
                 total_range += forward_search(index.text.begin(), index.text.end(), index.wt, 0, index.wt.size()-1, pat.subpatterns[i].begin(), pat.subpatterns[i].end(), sp, ep);
 
             return std::to_string(total_range);
         }
-        void prepare(const gapped_pattern& pat) { (void)pat; }
+        void prepare(const gapped_pattern& pat) 
+        { 
+            (void)pat;
+            if (index.text.width() <= 8)
+                text = string(index.text.begin(), index.text.end());
+        }
 
         gapped_search_result
         search(const gapped_pattern& pat) const
@@ -86,6 +89,38 @@ class index_wcsearch
                 // (this is an important concept, as it allows single-term matching by setting min/max_gap=0)
             }
 
+            // linear scan?
+            if (text.size() > 0)
+            {
+                index_type::size_type total_range = 0, sp = 0, ep = 0;
+                for (size_t i = 0; i < pat.subpatterns.size(); ++i)
+                    total_range += forward_search(index.text.begin(), index.text.end(), index.wt, 0, index.wt.size()-1, pat.subpatterns[i].begin(), pat.subpatterns[i].end(), sp, ep);
+                
+                // check for: |range| * log n > n
+                LOG(INFO) << "range = " << total_range << "; |text| = " << text.size();
+                total_range *= sdsl::bits::hi(text.size());
+                total_range *= CONST_LINEAR_THRESH;
+                LOG(INFO) << total_range << " > " << text.size() << " ==> " << (total_range > text.size());
+
+                if (total_range > text.size())
+                {
+                    // linear scan
+                    auto rx = boost::regex(pat.raw_regexp.begin(),pat.raw_regexp.end(),std::regex::ECMAScript);
+                    auto matches_begin = boost::sregex_iterator(
+                        text.begin(),
+                        text.end(),
+                        rx,
+                        boost::regex_constants::match_flag_type::match_not_dot_newline);
+                    auto matches_end = boost::sregex_iterator();
+
+                    for (boost::sregex_iterator it = matches_begin; it != matches_end; ++it) {
+                        res.positions.push_back(it->position());
+                    }
+                    return res;
+                }
+            }
+
+            // smart scan
             sdsl::PERFCTR_NUM_PROCESSED_WT_NODES = 1;
             for (auto hit : index.match(s1, s2, min_gap, max_gap)) {
                 res.positions.push_back(hit.first);
