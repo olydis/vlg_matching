5d9ff0b88db1b38d8f71cbd252450d9531f94516
Johannes Bader <jbader@i10pc127.iti.kit.edu>
2015-11-27 15:33:27 +0100

    current state

--- benchmark/gapped-matching/include/index_types.hpp
+++ benchmark/gapped-matching/include/index_types.hpp
@@ -3,11 +3,11 @@
 #define NUM_PATTERNS 15
 
 #include "index_regexp.hpp"
-#include "index_regexp_boost.hpp"
-#include "index_wcsearch.hpp"
+//#include "index_regexp_boost.hpp"
+//#include "index_wcsearch.hpp"
 #include "index_wcsearch3.hpp"
-#include "index_wcsearch_full.hpp"
-#include "index_wcsearch_bfs.hpp"
-#include "index_dbsearch.hpp"
+//#include "index_wcsearch_full.hpp"
+//#include "index_wcsearch_bfs.hpp"
+//#include "index_dbsearch.hpp"
 #include "index_sasearch.hpp"
-#include "index_qgram_regexp.hpp"
+//#include "index_qgram_regexp.hpp"
