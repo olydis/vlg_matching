bdf087bfa51f9d15592e376d9a881b64b3292960
Johannes <jogus@live.de>
2016-01-15 19:11:01 +0100

    bstree

--- benchmark/gapped-matching/include/index_types.hpp
+++ benchmark/gapped-matching/include/index_types.hpp
@@ -2,7 +2,7 @@
 
 #define NUM_PATTERNS 16
 
-#include "index_regexp.hpp"
+//#include "index_regexp.hpp"
 //#include "index_regexp_boost.hpp"
 //#include "index_wcsearch.hpp"
 #include "index_wcsearch3.hpp"
@@ -10,4 +10,5 @@
 //#include "index_wcsearch_bfs.hpp"
 //#include "index_dbsearch.hpp"
 #include "index_sasearch.hpp"
+#include "index_bstree.hpp"
 //#include "index_qgram_regexp.hpp"
